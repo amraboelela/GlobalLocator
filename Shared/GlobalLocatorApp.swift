87c40a306a9730c02bcbadf9c757c2fb51cd4432
Amr Aboelela <amraboelela@gmail.com>
2021-06-14 19:50:53 -0700

    Initial files

---
+++ Shared/GlobalLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalLocatorApp.swift
+//  Shared
+//
+//  Created by Amr Aboelela on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GlobalLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
