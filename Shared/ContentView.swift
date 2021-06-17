284c27fa0aa14e490107db69326dc0905b31277a
Amr Aboelela <amraboelela@gmail.com>
2021-06-16 20:25:01 -0700

    .onChange(of: region.center.longitude)

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,19 +7,61 @@
 
 import SwiftUI
 import MapKit
+import GlobalLocator
 
 struct ContentView: View {
-    
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State var gl: String = ""
+    @State var currentGL: String = ""
     
     var body: some View {
-        Map(coordinateRegion: $region)
-            .frame(width: 400, height: 300)
+        VStack(alignment: .leading, spacing: 10) {
+            Spacer()
+            HStack(spacing: 10) {
+                Text("Enter GL:")
+                    .padding(.leading)
+                TextField("", text: $gl)
+                    .frame(width: 150, height: 20)
+                    .textCase(.uppercase)
+                    .onChange(of: gl) {_ in
+                        gl = gl.uppercased()
+                    }
+                Button("Go") {
+                    let theCodes = gl.split(separator: " ")
+                    if theCodes.count == 2 {
+                        self.region = MKCoordinateRegion(center: globalLocator.locationFor(code: gl), span: globalLocator.spanFor(code: gl))
+                    }
+                }
+            }
+            //Spacer()
+            HStack(spacing: 10) {
+                Text("Current GL:  " + currentGL)
+                    .padding(.leading)
+                Spacer()
+                Button(action: {  }) {
+                    Image(systemName: "doc.on.doc")
+                        //.imageScale(.large)
+                        .accessibility(label: Text("Copy GL"))
+                        //.padding()
+                }
+                Spacer()
+            }
+            Map(coordinateRegion: $region)
+                .onChange(of: region.center.longitude) {_ in
+                    print("center: \(region.center)")
+                    print("span: \(region.span)")
+                    currentGL = globalLocator.codeFor(region: region)
+                }
+                /*.onChange(of: region.center.longitude) {_ in
+                    print("longitude: \(region.center.longitude)")
+                    currentGL = globalLocator.codeFor(region: region)
+                }*/
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(gl: "GZNF3 RKJ2G")
     }
 }
--- Shared/GlobalLocatorApp.swift
+++ Shared/GlobalLocatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GlobalLocatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(gl: "GZ RK")
         }
     }
 }
