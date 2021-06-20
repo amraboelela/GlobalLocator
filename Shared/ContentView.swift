988e44f68a56401fadb04411a2eaeea31dd15f6a
Amr Aboelela <amraboelela@gmail.com>
2021-06-20 13:00:24 -0700

    new file:   GlobalLocator.xcodeproj

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,43 +6,70 @@
 //
 
 import SwiftUI
+import AmrSwiftUI
 import MapKit
-import GlobalLocator
+import GlobalLocatorLib
+import MobileCoreServices
 
 struct ContentView: View {
-    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+    )
     @State var gl: String = ""
     @State var currentGL: String = ""
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Spacer()
             HStack(spacing: 10) {
-                Text("Enter GL:")
-                    .padding(.leading)
-                TextField("", text: $gl)
-                    .frame(width: 150, height: 20)
-                    .textCase(.uppercase)
-                    .onChange(of: gl) {_ in
-                        gl = gl.uppercased()
+                SearchBar(
+                    searchText: $gl,
+                    startSearchCallback: {
+                        print("startSearchCallback")
+                        if globalLocatorLib.isGLCode(text: gl) {
+                            gl = gl.uppercased()
+                            self.region = MKCoordinateRegion(
+                                center: globalLocatorLib.locationFor(code: gl),
+                                span: globalLocatorLib.spanFor(code: gl)
+                            )
+                        } else {
+                            globalLocatorLib.regionFor(query: gl, fromRegion: region) { matchingItem, resultRegion in
+                                region = resultRegion
+                            }
+                        }
+                    }, updateDataCallback: {
+                        print("updateDataCallback")
                     }
-                Button("Go") {
-                    let theCodes = gl.split(separator: " ")
-                    if theCodes.count == 2 {
-                        self.region = MKCoordinateRegion(center: globalLocator.locationFor(code: gl), span: globalLocator.spanFor(code: gl))
+                )
+                .padding(.leading)
+                //Spacer()
+                Button(
+                    action: {
+                        globalLocatorLib.regionFor(query: gl, fromRegion: region) { matchingItem, resultRegion in
+                            //region = result
+                            matchingItem?.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDefault])
+                        }
                     }
+                ) {
+                    Image(systemName: "arrow.uturn.forward.square")
+                        .accessibility(label: Text("Direction to location"))
                 }
+                Spacer()
             }
-            //Spacer()
             HStack(spacing: 10) {
                 Text("Current GL:  " + currentGL)
                     .padding(.leading)
                 Spacer()
-                Button(action: {  }) {
+                Button(
+                    action: {
+                        UIPasteboard.general.setValue(
+                            currentGL,
+                            forPasteboardType: kUTTypePlainText as String
+                        )
+                    }
+                ) {
                     Image(systemName: "doc.on.doc")
-                        //.imageScale(.large)
-                        .accessibility(label: Text("Copy GL"))
-                        //.padding()
+                        .accessibility(label: Text("Copy Global Locator"))
                 }
                 Spacer()
             }
@@ -50,12 +77,8 @@ struct ContentView: View {
                 .onChange(of: region.center.longitude) {_ in
                     print("center: \(region.center)")
                     print("span: \(region.span)")
-                    currentGL = globalLocator.codeFor(region: region)
+                    currentGL = globalLocatorLib.codeFor(region: region)
                 }
-                /*.onChange(of: region.center.longitude) {_ in
-                    print("longitude: \(region.center.longitude)")
-                    currentGL = globalLocator.codeFor(region: region)
-                }*/
         }
     }
 }
